3e05076708f5f2337fa18c2c81f2c917523afa1e
JWalasze <252897@student.pwr.edu.pl>
2022-04-22 13:00:09 +0200

    some changes

--- app/src/main/java/com/example/projekt_kalendarz/MainActivity.kt
+++ app/src/main/java/com/example/projekt_kalendarz/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.projekt_kalendarz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.month_view)
+
+    }
+
+
+
+
+    fun previousMonthAction(view: View) {
+
+    }
+
+    fun nextMonthAction(view: View) {
+
     }
 }
